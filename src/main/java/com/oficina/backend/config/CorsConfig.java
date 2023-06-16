832650faa4e9800529fdb58a8c778b943992ee34
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-06-16 20:26:58 -0300

    Config CORS

---
+++ src/main/java/com/oficina/backend/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.oficina.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+  @Value("${cors.origins}")
+  private String corsOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry){
+    registry.addMapping("/**")
+    .allowedOrigins(corsOrigins)
+    .allowedMethods("*");
+  }
+}
+
