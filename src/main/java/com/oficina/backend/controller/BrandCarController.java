b5da579ca23b0c15220079f63c7b24dbeb26f6b2
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-06-16 18:10:30 -0300

    CorOrigin config

--- src/main/java/com/oficina/backend/controller/BrandCarController.java
+++ src/main/java/com/oficina/backend/controller/BrandCarController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/brands")
-@CrossOrigin("http://localhost:5173")
 public class BrandCarController {
   
   @Autowired
